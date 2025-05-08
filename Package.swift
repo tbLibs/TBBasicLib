ddf5806fd93f08e2308822f93c0150d4315f90be
陶博 <691075311@qq.com>
2025-05-08 16:49:47 +0800

    支持spm

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "TBBasicLib",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "TBBasicLib",
+            targets: ["TBBasicLib"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/scalessec/Toast-Swift.git", from: "5.0.0")
+    ],
+    targets: [
+        .target(
+            name: "TBBasicLib",
+            dependencies: ["Toast-Swift"],
+            path: "TBBasicLib/Classes"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
+
